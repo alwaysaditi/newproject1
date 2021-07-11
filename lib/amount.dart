74d5b9f68b2f92f83686a23681f785a688550c2a
alwaysaditi <68003668+alwaysaditi@users.noreply.github.com>
2021-07-11 15:10:43 +0530

    TextField added and UI updated

--- lib/amount.dart
+++ lib/amount.dart
@@ -12,28 +12,32 @@ class Amount extends StatefulWidget {
   _AmountState createState() => _AmountState();
 }
 
-name() {
+name(double amount1) {
   int i;
   for (i = 0; i < 1; i++) {
-    streamController.add((5000).toDouble());
+    streamController.add(amount1.toDouble());
   }
 }
 
-addsomething() {
+addsomething(double amount2) {
   int j;
   for (j = 0; j < 1; j++) {
     print("add something was called");
     String rname = arr[choiceofreceivers - 1].name;
     String sname = arr[choiceofsender - 1].name;
-    double amount1 = 5000.toDouble();
+    double amount1 = amount2.toDouble();
     Transfers obj = Transfers(rname, sname, amount1);
 
     transferstream.add(obj);
-  
   }
 }
 
 class _AmountState extends State<Amount> {
+  final myController = TextEditingController();
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +46,7 @@ class _AmountState extends State<Amount> {
           // the App.build method, and use it to set our appbar title.
           title: Center(child: Text('Banking Application')),
           backgroundColor: Colors.teal[200]),
+      backgroundColor: Colors.blue,
       body: Builder(
         builder: (context) => Container(
             constraints: BoxConstraints.expand(),
@@ -52,11 +57,32 @@ class _AmountState extends State<Amount> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: name, child: Text('thats me')),
-                Text("money to be transerred from" +
-                    arr[choiceofsender - 1].name +
-                    "\n to" +
-                    arr[choiceofreceivers - 1].name),
+                Center(
+                  child: Text(
+                      "Enter the amount of money to be transerred from " +
+                          arr[choiceofsender - 1].name +
+                          "\n to " +
+                          arr[choiceofreceivers - 1].name),
+                ),
+                
+                Container(child: TextField(controller: myController),
+                margin: EdgeInsets.all(30)),
+              SizedBox(height: 20),
+                Center(
+                  child: Row(children: [ ElevatedButton(
+                    onPressed: () {
+                      name(5000.toDouble());
+                    },
+                    child: Text('Confirm'),
+                  ),
+                  
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                      onPressed :() {addsomething(5000.toDouble());},
+                      child: Text('Make Payment')),],),
+                ),
+               
+                SizedBox(height: 10),
                 Center(
                   child: ElevatedButton(
                     onPressed: () {
@@ -64,9 +90,7 @@ class _AmountState extends State<Amount> {
                     },
                     child: Text('Go back!'),
                   ),
-                ),
-                ElevatedButton(
-                    onPressed: addsomething, child: Text('add transfer'))
+                )
               ],
             )),
       ),
